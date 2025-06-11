14359eb3bd438784e8d708252d2cb09000b6f880
Howsco <1120132909@qq.com>
2025-06-11 11:27:56 +0800

    备份02一个简单的案例

---
+++ hello_vue3/src备份/02_一个简单的案例/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">展示姓名</button>
+    <button @click="changeAge">展示年龄</button>
+    <button @click="showTel">展示电话号码</button>
+  </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Person',
+        data() {
+            return {
+                name: '张三',
+                age: 18,
+                tel: '18787878899'
+            }
+        },
+        methods: {
+            showTel() {
+                alert(this.tel)
+            },
+            changeName() {
+                this.name = 'zhang-san'
+            },  
+            changeAge() {
+                this.age += 1
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+
+    button {
+        margin: 0 5px;
+    }
+</style>
