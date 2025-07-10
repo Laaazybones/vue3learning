d0795bfc36c259b4b32ce190de4619218a82b7c9
Howsco <1120132909@qq.com>
2025-07-10 10:27:48 +0800

    06 setup语法糖

---
+++ hello_vue3/src备份/05_setup与OptionsAPI的关系/components/Person.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="person">
+    <!-- <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">展示姓名</button>
+    <button @click="changeAge">展示年龄</button>
+    <button @click="showTel">展示电话号码</button> -->
+  </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Person',
+        data() {
+            return {
+                name: "zhang-san",
+                age: 19,
+                telephone: this.tel // vue2可以引用vue3中定义的变量，但是vue3不能反过来引用vue2定义的变量
+            }
+        },
+        methods: {
+            aFun() {
+                console.log('aFun')
+            }
+        },
+        setup() {
+            // console.log(this)    setup函数中的this是undefined，Vue3中已经弱化了this
+            // setup 中定义的属性是非响应式的
+            let name = '张三'
+            let age = 18
+            let tel = '13899990000'
+
+            // 这样可以修改属性值，但是不是响应式，不会直接在页面上展示
+            function changeName() {
+                console.log('1')
+                name = 'zhang-san'
+                console.log(name)
+            }
+
+            function changeAge() {
+                console.log('2')
+                age += 1
+                console.log(age)
+            }
+
+            function showTel() {
+                alert(tel)
+            }
+
+            // return { name, age, changeName, changeAge, showTel }
+            // setup的返回值也可以是一个渲染函数
+            return () => '哈哈'
+        }
+    }
+</script>
+
+<style scoped>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+
+    button {
+        margin: 0 5px;
+    }
+</style>
--- hello_vue3/vite.config.ts
+++ hello_vue3/vite.config.ts
@@ -3,12 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 将vue3两组script标签写在一起的插件
+import vueSetupExtend from 'vite-plugin-vue-setup-extend' 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    vueSetupExtend()
   ],
   resolve: {
     alias: {
